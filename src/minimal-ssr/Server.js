ed0a60718dbcc7158a64039331cb257379528306
nathb <nathb2b@gmail.com>
2018-10-12 10:46:23 +0200

    ^ 2.0.0-alpha

--- src/minimal-ssr/Server.js
+++ src/minimal-ssr/Server.js
@@ -3,7 +3,7 @@ var express      = require("express"),
     App          = require('./dist/App.server').default,
     fs           = require("fs"),
     server       = express(),
-    currentState = {},
+    currentState = null,
     http         = require('http').Server(server),
     argz         = require('minimist')(process.argv.slice(2));
 
@@ -11,31 +11,31 @@ server.use(express.json());       // to support JSON-encoded bodies
 server.use(express.urlencoded()); // to support URL-encoded bodies
 
 server.get(
-    '/',
-    function ( req, res, next ) {
-        App.renderSSR(
-            {
-                url  : req.url,
-                state: currentState
-            },
-            ( err, html ) => {
-                !err ?
-                res.send(200, html)
-                     :
-                res.send(500, err + '')
-            }
-        )
-    }
+	'/',
+	function ( req, res, next ) {
+		App.renderSSR(
+			{
+				url  : req.url,
+				state: currentState
+			},
+			( err, html ) => {
+				!err ?
+				res.send(200, html)
+				     :
+				res.send(500, err + '')
+			}
+		)
+	}
 );
 
 server.post('/', function ( req, res, next ) {
-    console.log("New state pushed")
-    currentState = req.body;
-    res.send(200, 'ok')
+	console.log("New state pushed")
+	currentState = req.body;
+	res.send(200, 'ok')
 });
 
 server.use(express.static('./dist'))
 
 var server_instance = http.listen(parseInt(argz.p || argz.port || 8000), function () {
-    console.warn('Running on ', server_instance.address().port)
+	console.warn('Running on ', server_instance.address().port)
 });
